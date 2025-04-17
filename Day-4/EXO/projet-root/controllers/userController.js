2a5cb0cb4ee90696a496eb8e1b4376f5e02b9d53
KAMsdu91 <kamdemlionel24@gmail.com>
2025-04-17 17:54:17 +0200

    cinquieme Etape:contenue du fichier userController.js

---
+++ Day-4/EXO/projet-root/controllers/userController.js
@@ -0,0 +1,21 @@
+const users = require('../models/userModel');
+
+exports.getMe = (req, res) => {
+  const user = users.find(u => u.id === req.user.id);
+  if (!user) return res.status(404).json({ message: 'User not found' });
+  res.json({ email: user.email });
+};
+
+exports.getAllUsers = (req, res) => {
+  if (!req.user.isAdmin) {
+    return res.status(403).json({ message: 'Access denied' });
+  }
+  res.json(users.map(({ password, ...rest }) => rest));
+};
+
+exports.deleteUser = (req, res) => {
+  const index = users.findIndex(u => u.id === req.params.id);
+  if (index === -1) return res.status(404).json({ message: 'User not found' });
+  users.splice(index, 1);
+  res.json({ message: 'User deleted' });
+};
