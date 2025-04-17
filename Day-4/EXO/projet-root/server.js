030c7fd5cef8afb00d7e759d6bcd0f72383082e4
KAMsdu91 <kamdemlionel24@gmail.com>
2025-04-17 17:24:45 +0200

    premier Etape:configuration du server.je

---
+++ Day-4/EXO/projet-root/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+app.use(express.json());
+
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
