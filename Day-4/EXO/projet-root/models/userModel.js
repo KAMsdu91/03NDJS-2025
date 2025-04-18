c6fb5f929829832aacf4b0f751b3830298f11123
KAMsdu91 <kamdemlionel24@gmail.com>
2025-04-18 09:42:24 +0200

    Installation de mongoodb et modification du userModel.js

--- Day-4/EXO/projet-root/models/userModel.js
+++ Day-4/EXO/projet-root/models/userModel.js
@@ -1,2 +1,10 @@
-let users = []; // stockage en mémoire
-module.exports = users;
+// models/User.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false }
+});
+
+module.exports = mongoose.model('User', userSchema);
